271a83a673622a089e2bc0a6bbec03b3ba6b2f7d
José V S Carneiro <git@josevaltersilvacarneiro.net>
2024-04-03 18:58:30 +0000

    Stylizing the code

--- Migrations/00_users.sql
+++ Migrations/00_users.sql
@@ -18,4 +18,4 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-CREATE USER 'app'@'%' IDENTIFIED BY 'kaksqlkaalksldalkdn' ;
+CREATE USER 'app'@'%' IDENTIFIED BY 'kaksqlkaalksldalkdn';
