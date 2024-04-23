65e709067569298814b3f6346e409661d1401880
José V S Carneiro <git@josevaltersilvacarneiro.net>
2024-04-23 14:09:37 +0000

    Adding validation

--- App/Controller/Ticket/NewTicket.php
+++ App/Controller/Ticket/NewTicket.php
@@ -53,7 +53,7 @@ use Psr\Http\Message\ServerRequestInterface;
  * @author    José Carneiro <git@josevaltersilvacarneiro.net>
  * @copyright 2023 José Carneiro
  * @license   GPLv3 https://www.gnu.org/licenses/quick-guide-gplv3.html
- * @version   Release: 0.0.1
+ * @version   Release: 0.0.2
  * @link      https://github.com/josevaltersilvacarneiro/html/tree/main/App/Cotrollers
  */
 final class NewTicket extends HTMLController
@@ -95,8 +95,11 @@ final class NewTicket extends HTMLController
 
         $stmt = $repository->query($query);
 
+        $today = new \DateTimeImmutable();
+
         $this->setVariables([
-            'SUPPLIERS_LIST' => $stmt === false ? [] : $stmt->fetchAll(\PDO::FETCH_ASSOC)
+            'SUPPLIERS_LIST' => $stmt === false ? [] : $stmt->fetchAll(\PDO::FETCH_ASSOC),
+            'TODAY_DATE_' => $today->format('Y-m-d')
         ]);
 
         return new Response(
