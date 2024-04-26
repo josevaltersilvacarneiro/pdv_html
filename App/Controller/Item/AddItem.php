3eb2a3bf4520085c6740b7c5fd5b85dd22fd1427
José V S Carneiro <git@josevaltersilvacarneiro.net>
2024-04-26 13:39:41 +0000

    Validating the inputs

--- App/Controller/Item/AddItem.php
+++ App/Controller/Item/AddItem.php
@@ -52,7 +52,7 @@ use Psr\Http\Server\RequestHandlerInterface;
  * @author    José Carneiro <git@josevaltersilvacarneiro.net>
  * @copyright 2023 José Carneiro
  * @license   GPLv3 https://www.gnu.org/licenses/quick-guide-gplv3.html
- * @version   Release: 0.1.2
+ * @version   Release: 0.1.3
  * @link      https://github.com/josevaltersilvacarneiro/html/tree/main/App/Cotrollers
  */
 final class AddItem implements RequestHandlerInterface
@@ -85,9 +85,19 @@ final class AddItem implements RequestHandlerInterface
         // getting the parameters
 
         $title = filter_input(INPUT_POST, 'title');
+
+        if ($title === false || is_null($title)) {
+            return new Response(302, ['Location' => '/failed']);
+        }
+
         $title = mb_convert_case($title, MB_CASE_TITLE, 'UTF-8');
 
         $price = filter_input(INPUT_POST, 'price');
+
+        if ($price || is_null($price) || !preg_match('/^(0|[1-9]\d{0,2}(\.\d{3})*),\d{2}$/', $price)) {
+            return new Response(302, ['Location' => '/failed']);
+        }
+
         $price = mb_ereg_replace('\.', '', $price);
         $price = mb_ereg_replace(',', '.', $price);
 
